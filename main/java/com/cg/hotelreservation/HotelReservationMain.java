b0886f1831a57b0957dff1b8732b9cfa4e4101a7
praveen3073 <pop3073@gmail.com>
2020-11-16 14:59:43 +0530

    Added: Find Cheapest Best Rated Hotel In Hotel List

--- main/java/com/cg/hotelreservation/HotelReservationMain.java
+++ main/java/com/cg/hotelreservation/HotelReservationMain.java
@@ -20,13 +20,27 @@ public class HotelReservationMain {
         return null;
     }
 
-    // Find cheapest hotel by weekday rates in between the given start and end date
+    // Find cheapest hotel by weekend rates in between the given start and end date
     public Hotel findCheapestHotelByWeekendRates(String startDate, String endDate) {
         Optional<Hotel> cheapestHotel = hotelList.stream()
                 .filter(h -> h.startDate.compareTo(startDate) > 0 && h.endDate.compareTo(endDate) < 0)
                 .reduce((hotel1, hotel2) -> hotel1.weekendRate < hotel2.weekendRate ? hotel1 : hotel2);
         if (cheapestHotel.isPresent())
             return cheapestHotel.get();
+        return null;
+    }
+
+    // Find cheapest best rated hotel by weekday rates in given date range
+    public Hotel findCheapestBestRatedHotelByWeekdayRates(String startDate, String endDate) {
+        double bestRating = hotelList.stream()
+                .filter(h -> h.startDate.compareTo(startDate) > 0 && h.endDate.compareTo(endDate) < 0)
+                .reduce((hotel1, hotel2) -> hotel1.rating > hotel2.rating ? hotel1 : hotel2).get().rating;
+        Optional<Hotel> cheapestBestRatedHotel = hotelList.stream()
+                .filter(h -> h.startDate.compareTo(startDate) > 0 && h.endDate.compareTo(endDate) < 0)
+                .filter(h -> h.rating == bestRating)
+                .reduce((hotel1, hotel2) -> hotel1.weekdayRate < hotel2.weekdayRate ? hotel1 : hotel2);
+        if (cheapestBestRatedHotel.isPresent())
+            return cheapestBestRatedHotel.get();
         return null;
     }
 }
